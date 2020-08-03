5b3d194f7e00076fe081d7aac420dbf04dd673f6
Khalil Carsten <khalilcarsten@gmail.com>
2020-08-03 02:52:09 -0300

    Well I hope this works

---
+++ insiders/insider.py
@@ -0,0 +1,49 @@
+from keras.models import load_model
+from decimal import Decimal
+from pkg_resources import resource_filename, Requirement
+from insiders.models.denselstm import DenseLSTM
+import numpy
+from b3data.utils.stock_util import StockUtil
+
+class Insider:
+
+    def __init__(self, code, look_back, cells):
+        self.model = self._create_model(code, look_back, cells)
+        # self.model = load_model(resource_filename(Requirement.parse("insiders"), 'saved_models/' + code + '.h5'))
+
+    def _create_model(self, code, look_back, cells):
+        lstm = DenseLSTM(look_back=look_back, lstm_cells=cells, optimizer='rmsprop')
+        lstm.model.load_weights('insiders/saved_models/' + code + '.h5') 
+        
+        return lstm
+
+    def predict(self, dataset):
+        dataset = numpy.array(dataset)
+        dataset = dataset.reshape(-1, 1)
+        self.model.create_data_for_fit(dataset)
+        predictions = self.model.model.predict(numpy.append(self.model.train_x, self.model.test_x, 0))
+        prediction_labels = [1 if Decimal(i.item()) >= Decimal(0.50) else 0 for i in predictions]
+        score = 0
+        for i in range(len(prediction_labels)):
+            if prediction_labels[i] == numpy.append(self.model.train_y, self.model.test_y)[i]:
+                score += 1
+        
+        print('[Insider] Accuracy: ' + str(score/len(prediction_labels)*100))
+        return prediction_labels
+
+    @staticmethod
+    def stock_and_predicts(codes=['PETR3'], windows=[6], start_year=2014, end_year=2014,
+                          start_month=1,
+                          period=6):
+
+        s = StockUtil(codes, windows)
+        prices, _ = s.prices_preds(2014, 2014)
+        cells = {'PETR3': 50, 'VALE3': 50, 'ABEV3': 50}
+        preds = []
+        for index, code in enumerate(codes):
+            ins = Insider(code, windows[index], cells[code])
+            preds.append(ins.predict(prices[index]))
+        
+        return prices, preds
+
+# prices, preds = Insider.stock_and_predicts(['PETR3', 'VALE3', 'ABEV3'], [6, 6, 9], start_month=7)
--- scripts/plot_profit.py
+++ scripts/plot_profit.py
@@ -22,10 +22,16 @@ stocks = Stocks(year=2014, cod=code, period=6)
 dataset = stocks.selected_fields([CLOSING])
 dataset = duplicate_data(dataset)
 
+print(dataset)
+
 model = DenseLSTM(input_shape=dataset.shape[1],
                   look_back=look_back, lstm_cells=lstm_cells, optimizer=optimizer)
 model.create_data_for_fit(dataset)
+
+print(model.test_x)
 result = model.fit_and_evaluate(batch_size=2, epochs=5000)
+
+result['model'].save(code+'.h5')
 
 average_dataset = [dataset[i-look_back:i].mean() for i in range(look_back, len(dataset))]
 prediction = model.model.predict(model.test_x)
@@ -34,7 +40,7 @@ prediction_labels = [1 if Decimal(i.item()) >= Decimal(0.50) else 0 for i in pre
 begin_test = int(len(dataset) - len(model.test_x))
 correct_labels = [1 if dataset[i] >= dataset[i-look_back:i].mean() else 0 for i in range(begin_test, len(dataset))]
 
-plt.figure(figsize=(8, 3))
+plt.figure(figsize=(10, 3))
 
 x = list(range(len(model.test_x)))
 y = average_dataset[begin_test-look_back:len(average_dataset)]
@@ -59,20 +65,21 @@ for i in range(len(x)-1):
 
     plt.plot([x[i],x[i]], [y_d[i], y[i]], 'k--', linewidth=1)
 
-d_label, = plt.plot(y, color='#70a2ff')
-a_label, = plt.plot(y_d, '-o',color='#ff756b', markersize=3)
+a_label, = plt.plot(y, color='#70a2ff')
+d_label, = plt.plot(y_d, '-o',color='#ff756b', markersize=3)
 
-# extra = Rectangle((0, 0), 1, 1, fc="w", fill=False, edgecolor='none', linewidth=0)
+extra = Rectangle((0, 0), 1, 1, fc="w", fill=False, edgecolor='none', linewidth=0)
 # plt.legend((extra, d_label), ("Precisão: {}%".format(round(score/(len(x)-1), 2)*100), 
 #                               "Preço médio últimos " + str(look_back) + " dias"), fontsize=10)
 
-plt.legend((lx, hx, l, h), ("Precisão: {}%".format(round(score/(len, 
-                            "Preço médio últimos " + str(look_back) + " dias"), fontsize=10)
+plt.legend((extra, l, h, hx, a_label, d_label), 
+           ("Acurácia: {}%".format(round(score/(len(x)-1), 2)*100), 
+            "Predição correta de baixa",
+            "Predição correta de alta",
+            "Predição incorreta de alta",
+            "Preço médio dos últimos " + str(look_back) + " dias",
+            "Preço real do ativo"), fontsize=10, bbox_to_anchor=(1.05, 1))
 
-figure = legend.figure
-fig.canvas.draw()
-bbox = legend.get_window_extent().transformed(fig.dpi_scale_trans.inverted())
-fig.savefig(filename, dpi="figure", bbox_inches=bbox)
 
 plt.gca().spines['top'].set_visible(False)
 plt.gca().spines['right'].set_visible(False)
